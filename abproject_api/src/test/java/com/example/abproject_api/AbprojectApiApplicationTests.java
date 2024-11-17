832ae21bd9bd4a7de7709b116a3d8c5df7d236f6
박우진 <woojin18@hanmail.net>
2024-11-17 10:41:16 +0900

    초기 설정

---
+++ abproject_api/src/test/java/com/example/abproject_api/AbprojectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abproject_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbprojectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
